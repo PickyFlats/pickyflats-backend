6cc69ea466e38d4629dbde7fdd82c39e1dfa0d3f
Mahesh Bohara <13344241+itsbohara@users.noreply.github.com>
2023-06-23 09:28:12 +0545

    feat: updated auth data for token assign, auth guard route protection

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ForbiddenExceptionFilter } from './common/filters/forbidden-exception.f
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalFilters(new ForbiddenExceptionFilter());
   await app.listen(3000);
 }
